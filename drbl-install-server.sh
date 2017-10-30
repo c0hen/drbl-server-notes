c50acccdd538dab2cbf9651de86c7dbff95b6fb9
c0hen <tan.elrk@gmail.com>
2017-10-30 14:47:18 +0200

    Drbl initial content commit

---
+++ drbl-install-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#drblsrv -o 1 -k 2 -g n -c n -n n -i
+drblsrv -o 2 -k 2 -g n -c n -n n -i
---
+++ drblpush-create-filesystem-for-booting.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+drblpush -v -b -m 2 -o /home/partimag -s N -r 1 -u 2 -z 1 -t 1 -i --drbl-ocs-live-server 172.31.0.1
